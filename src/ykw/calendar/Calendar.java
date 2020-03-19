5db9f2d95d32ab24e070c680bc3b7bb69d42a8ea
user <user@BIT04-11>
2020-03-19 18:25:41 +0900

    leap year

--- src/ykw/calendar/Calendar.java
+++ src/ykw/calendar/Calendar.java
@@ -11,25 +11,28 @@ public class Calendar {
 	3 (엔터)
 	3월은 31일까지 있습니다.
 	 */
-	private static final int[] MAX_DAYS ={ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	public int getMaxDaysOfMonth(int month){
-		return MAX_DAYS[month-1];
-	}
-	public static void month(){
-		
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-		
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (year % 4 == 0 && month == 2) {
+			MAX_DAYS[month - 1] = 29;
+			return MAX_DAYS[month - 1];
+		}
+		return MAX_DAYS[month - 1];
 	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		System.out.println("달을 입력하세요.");
-		int month = sc.nextInt();
-		
-		
-		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
-		
+		System.out.println("년도,달 입력하세요.");
+		String num = sc.nextLine();
+
+		String[] numlist = num.split(",");
+		int year = Integer.parseInt(numlist[0]);
+		int month = Integer.parseInt(numlist[1]);
+
+		System.out.printf("%d년 %d월은 %d일까지 있습니다.", year, month, cal.getMaxDaysOfMonth(year, month));
 
 	}
 
