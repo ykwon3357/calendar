24727c88dfa075433f553be1e9020e9f332b3374
user <user@BIT04-11>
2020-03-19 17:57:49 +0900

    second

--- src/ykw/calendar/Calendar.java
+++ src/ykw/calendar/Calendar.java
@@ -1,9 +1,26 @@
 package ykw.calendar;
 
-public class Calendar {
+import java.util.Scanner;
 
+public class Calendar {
+	// 31 29 31 30 31 30 31 31 30 31 30 31
+	// 4년 마다 2월 29일
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// if 배열
+		/*
+		 * 달을 입력하세요.
+		3 (엔터)
+		3월은 31일까지 있습니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+		System.out.println("달을 입력하세요.");
+		int i=sc.nextInt();
+		
+		System.out.printf("%d월은 %d일까지 있습니다.",i,month[i-1]);
+		
+		
 
 	}
 
