ed66860f8dacb48f71f98fb70dcf2f6932d10d77
user <user@BIT04-11>
2020-03-19 18:08:53 +0900

    third

--- src/ykw/calendar/Calendar.java
+++ src/ykw/calendar/Calendar.java
@@ -11,15 +11,25 @@ public class Calendar {
 	3 (엔터)
 	3월은 31일까지 있습니다.
 	 */
-	public static void main(String[] args) {
+	private static final int[] MAX_DAYS ={ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	public int getMaxDaysOfMonth(int month){
+		return MAX_DAYS[month-1];
+	}
+	public static void month(){
+		
 
+		
+	}
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] month = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		Calendar cal = new Calendar();
 
 		System.out.println("달을 입력하세요.");
-		int i = sc.nextInt();
-
-		System.out.printf("%d월은 %d일까지 있습니다.", i, month[i - 1]);
+		int month = sc.nextInt();
+		
+		
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
+		
 
 	}
 
