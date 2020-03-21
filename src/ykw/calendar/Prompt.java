61d56913ad521a0566ccdb140f16cc8232b2cc1b
유경원 <@DESKTOP-QU5R6U9>
2020-03-21 23:21:18 +0900

    일정 검색,등록 추가

--- src/ykw/calendar/Prompt.java
+++ src/ykw/calendar/Prompt.java
@@ -1,11 +1,74 @@
 package ykw.calendar;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Prompt {
+	static Scanner sc = new Scanner(System.in);
+	public void todoList() {
+		String command = "", yearday="",content="",search="";
+		Map<String, String> yc = new HashMap<String, String>();
+		String[] contents= {};
+		int year, month, day;
+		System.out.println("+------------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말 q.종료");
+		System.out.println("+------------------+");
+		
+		do {
+		System.out.printf("명령(1,2,3,h,q)\n>");
+		command=sc.nextLine();
+		
+		if(command.equals("1")) {
+			System.out.printf("[일정 등록] 날짜를 입력하세요.(2020-01-01)\n>");
+			yearday= sc.nextLine();
+	
+			/*String[] s=yearday.split("-");
+			year=Integer.parseInt(s[0]);
+			month=Integer.parseInt(s[1]);
+			day=Integer.parseInt(s[2]);
+			*/
+			
+			System.out.println("일정을 입력하세요.");
+			content=sc.nextLine();
+			System.out.println("일정이 등록되었습니다.");
+			if(yc.containsKey(yearday)) {
+				content=yc.get(yearday)+","+content;
+			}
+			yc.put(yearday, content);
+			
+		}
+		if(command.equals("2")) {
+			System.out.printf("[일정 검색] 날짜를 입력하세요.(2020-01-01)\n>");
+			search= sc.nextLine();
+			
+			if(yearday.equals(search)) {
+				contents=content.split(",");
+				System.out.printf("%d개의 일정이 있습니다.\n",contents.length);
+				for(int i=0;i<contents.length;i++) {
+					System.out.printf("%d. %s\n",i+1,contents[i]);
+				}
+			}
+			
+			
+			
+			
+		}
+		
+			
+		
+		
+		
+		}while(!command.equals("q"));
+		
+		System.out.println("Bye");
+		
+	}
 
 	public void runPrompt() {
-		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		int year = 2020, month = 1;
@@ -37,6 +100,8 @@ public class Prompt {
 
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
-		p.runPrompt();
+//		p.runPrompt();
+		
+		p.todoList();
 	}
 }
